16bb63881937390ffc456271b5583e490856b8e9
Aokifree98 <kinol787@gmail.com>
2023-10-09 10:53:10 -0500

    Estructuración de carpetas y conexión a la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeormService } from './services/conectiondb/conectiondb.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    //Importacion del servicio para conectar a la DB
+    TypeOrmModule.forRootAsync({useClass: TypeormService}),
+    //Importancion de los servicios
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //Prefijo fijo de utilizacion de apis
+  app.setGlobalPrefix("api/v1");
+  //Validacion Flobal
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      forbidNonWhitelisted:true,
+      transform:true,
+    })
+  );
+  app.enableCors();
+  //Puerto del servidor
+  await app.listen(4000);
 }
 bootstrap();
